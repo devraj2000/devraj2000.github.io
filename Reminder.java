270ec1b5b550b84e31ef47fc1d4d8fff8542fa0a
devil_inside <86217516+devraj2000@users.noreply.github.com>
2025-01-14 13:40:59 +0530

    Create Reminder.java

---
+++ Reminder.java
@@ -0,0 +1,17 @@
+public class Reminder {
+    private String phoneNumber;
+    private String message;
+
+    public Reminder(String phoneNumber, String message) {
+        this.phoneNumber = phoneNumber;
+        this.message = message;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
